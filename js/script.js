011fc019d574997d359ec5ab9bb51ce5e9551af8
Nghuynh07 <79157586+Nghuynh07@users.noreply.github.com>
2022-05-02 22:38:27 -0700

    deploy

--- js/script.js
+++ js/script.js
@@ -8,15 +8,36 @@ container.addEventListener("click", function (e) {
   }
 });
 
-const mouseover = (e) => {
-  if (e.target.classList.contains("feature")) {
-    e.target.children[1].style.display = "block";
-    e.target.children[1].style.transition = "all .3s";
-  }
-  if (e.target.classList.contains("company")) {
-    e.target.children[1].style.display = "block";
-    e.target.children[1].style.transition = "all .3s";
+const target = (object) => {
+  object.classList.toggle("display-nav");
+  if (object.classList.contains("display-nav")) {
+    object.parentElement.children[0].firstElementChild.attributes[0].nodeValue =
+      "images/icon-arrow-up.svg";
+  } else {
+    object.parentElement.children[0].firstElementChild.attributes[0].nodeValue =
+      "images/icon-arrow-down.svg";
   }
 };
 
-nav.addEventListener("mouseover", mouseover);
+nav.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("first-dropdown")) {
+    const feature = document.querySelector(".feature-dropdown");
+    target(feature);
+  } else if (e.target.classList.contains("second-dropdown")) {
+    const company = document.querySelector(".company-dropdown");
+    target(company);
+  } else {
+    return;
+  }
+
+  if (e.target.classList.contains("arrow")) {
+    const feature = document.querySelector(".feature-dropdown");
+    target(feature);
+  } else if (e.target.classList.contains("arrow")) {
+    const company = document.querySelector(".company-dropdown");
+    target(company);
+  } else {
+    return;
+  }
+});
